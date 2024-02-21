110650adb588b2b9a6575a0710b768255bc8d50f
kappy <shangenjunfu9@gmail.com>
2024-02-21 16:38:53 +0900

    trainBasic

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,6 +3,7 @@ import Link from 'next/link'
 import React, { useState, useEffect } from 'react';
 
 const busSchedule = ['13:00', '14:00', '15:00'];
+const trainSchedule = ['13:00', '14:00', '15:00', '16:00', '17:00', '18:00'];
 
 export default function Home() {
   const Clock = () => {
@@ -25,7 +26,7 @@ export default function Home() {
       busDate.setHours(busHour, busMinute, 0, 0);
       return busDate > time;
     });
-    let timeDifference = 'No more buses today';
+    let bustimeDifference = '本日のバスの運行は終了しました。';
     if (nextBusTime) {
       const [busHour, busMinute] = nextBusTime.split(':').map(Number);
       const nextBusDate = new Date();
@@ -34,15 +35,34 @@ export default function Home() {
       const hours = Math.floor(diffSeconds / 3600);
       const minutes = Math.floor((diffSeconds % 3600) / 60);
       const seconds = diffSeconds % 60;
-      timeDifference = `次のバスまで ${hours}時間${minutes}分${seconds}秒`;
+      bustimeDifference = `次のバスまで ${hours}時間${minutes}分${seconds}秒`;
     }
 
+    // 次の電車までの時間を計算
+    const nextTrainTime = trainSchedule.find(trainTime => {
+      const [trainHour, trainMinute] = trainTime.split(':').map(Number);
+      const trainDate = new Date();
+      trainDate.setHours(trainHour, trainMinute, 0, 0);
+      return trainDate > time;
+    });
+    let traintimeDifference = '本日の電車の運行は終了しました。';
+    if (nextTrainTime) {
+      const [trainHour, trainMinute] = nextTrainTime.split(':').map(Number);
+      const nextTrainDate = new Date();
+      nextTrainDate.setHours(trainHour, trainMinute, 0, 0);
+      const diffSeconds = Math.round((nextTrainDate - time) / 1000);
+      const hours = Math.floor(diffSeconds / 3600);
+      const minutes = Math.floor((diffSeconds % 3600) / 60);
+      const seconds = diffSeconds % 60;
+      traintimeDifference = `次の電車まで ${hours}時間${minutes}分${seconds}秒`;
+    }
 
     return (
       <div>
         <h2>{time.toLocaleDateString()}</h2>
         <h2>{time.toLocaleTimeString()}</h2>
-        <h2>{timeDifference}</h2>
+        <h2>{bustimeDifference}</h2>
+        <h2>{traintimeDifference}</h2>
       </div>
     );
   };
